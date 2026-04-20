a7c3d5edf0ece7b4de6a8f2d421e60be3b5e349b
AuroBreeze <1732373074@aliyun.com>
2026-04-20 10:28:56 +0800

    fix: Move `handle_java` to the appropriate location and fix the circular import issue between `config` and `java`.

--- nexus-java/src/lib.rs
+++ nexus-java/src/lib.rs
@@ -1,1 +1,39 @@
 pub mod java;
+
+use crate::java::{download_java, scan_local_java_environments};
+use nexus_cli::cli::JavaArgs;
+use nexus_config::config::Config;
+use nexus_config::models::LaunchConfig;
+use nexus_core::AnyError;
+use nexus_core::get_minecraft_dir;
+
+pub async fn handle_java(args: &JavaArgs) -> Result<(), AnyError> {
+    if args.download {
+        let java_version = args.version;
+        let custom_runtime_dir = get_minecraft_dir().join("runtimes");
+        download_java(java_version, custom_runtime_dir.as_path()).await?;
+    }
+
+    if args.scan {
+        tracing::info!("📦 Scanning local Java environments...");
+
+        let mut config = LaunchConfig::load().await;
+        let local_javas = scan_local_java_environments(None).await;
+
+        tracing::info!("📦 Found {} Java environments:", local_javas.len());
+        for j in local_javas {
+            tracing::info!(
+                "📦 Found Java {} (full version: {}) -> Path: {}",
+                j.major_version,
+                j.full_version,
+                j.path.display()
+            );
+
+            config.java_paths.insert(j.major_version, j.path.clone());
+        }
+
+        config.save().await?;
+    }
+
+    Ok(())
+}
--- nexus-main/src/main.rs
+++ nexus-main/src/main.rs
@@ -10,6 +10,7 @@ use nexus_config::config::Config;
 use nexus_config::handle_set;
 use nexus_config::models::{LaunchConfig, UserConfig};
 
+use nexus_java::handle_java;
 use nexus_java::java::{download_java, scan_local_java_environments};
 
 use nexus_launch::launcher::start_game;
@@ -344,38 +345,6 @@ async fn handle_launch(args: &LaunchArgs) -> Result<(), AnyError> {
     };
 
     start_game(launch_context)?;
-
-    Ok(())
-}
-
-// TODO: Migrate code
-async fn handle_java(args: &JavaArgs) -> Result<(), AnyError> {
-    if args.download {
-        let java_version = args.version;
-        let custom_runtime_dir = get_minecraft_dir().join("runtimes");
-        download_java(java_version, custom_runtime_dir.as_path()).await?;
-    }
-
-    if args.scan {
-        tracing::info!("📦 Scanning local Java environments...");
-
-        let mut config = LaunchConfig::load().await;
-        let local_javas = scan_local_java_environments(None).await;
-
-        tracing::info!("📦 Found {} Java environments:", local_javas.len());
-        for j in local_javas {
-            tracing::info!(
-                "📦 Found Java {} (full version: {}) -> Path: {}",
-                j.major_version,
-                j.full_version,
-                j.path.display()
-            );
-
-            config.java_paths.insert(j.major_version, j.path.clone());
-        }
-
-        config.save().await?;
-    }
 
     Ok(())
 }
